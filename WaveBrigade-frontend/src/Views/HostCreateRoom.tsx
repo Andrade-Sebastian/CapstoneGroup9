4689aff545e17dc36eba2b1914b538be2a15cfdc
Eman <98251644+emanp@users.noreply.github.com>
2024-10-30 17:54:20 -0700

    added spectator options and password to create room page

--- WaveBrigade-frontend/src/Views/HostCreateRoom.tsx
+++ WaveBrigade-frontend/src/Views/HostCreateRoom.tsx
@@ -10,7 +10,7 @@ export default function HostCreateRoom()
     const navigateTo = useNavigate();
     const [passwordIsSelected, setPasswordIsSelected] = useState(false);
     const [allowSpectators, setAllowSpectators] = useState(false);
-
+    const [password, setPassword] = useState("");
 
     function handleSubmit(e: { preventDefault: () => void; })
     {
@@ -27,17 +27,37 @@ export default function HostCreateRoom()
     console.log("Allow Spectators? " + allowSpectators);
     console.log("---------");
 
+    function handlePasswordSubmit(e)
+    {
+        if (passwordIsSelected)
+        {
+            //add: send the password to the server, 
+            //user should be able to join using that password
+            e.preventDefault();
+            console.log("Lobby Password: " + password);
+        }
+        
+
+    }
     
     return( 
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="userName"> Enter your name </label>
                 <input type="text" id="userName" onChange={(e) => setUserName(e.target.value)}></input>
-                <input type="submit" value={"Host Lobby"}></input> {/*This will redirect to Media Page */}
+                <input type="submit" value={"Host Lobby"} /> {/*This will redirect to Media Page */}
             </form>
             <div className="flex gap-4" >
                 <Checkbox size="sm" color="primary" onValueChange={() => setPasswordIsSelected(!passwordIsSelected)}>Use Password</Checkbox>
-                {passwordIsSelected ? <div><label htmlFor="Password">Password</label><input id="Password" type="text" >Password</input></div> : null}
+                {passwordIsSelected ? 
+                (   <div>
+                        <form onSubmit={handlePasswordSubmit}>
+                            <label htmlFor="Password">Password</label>
+                            <input id="Password" type="text" onChange={(e)=> setPassword(e.target.value)}/>
+                            <input type="submit" value={"Submit"} />
+                        </form>
+                    </div>
+                ) : null}
                 <Checkbox size="sm" color="primary" onValueChange={() => setAllowSpectators(!allowSpectators)}>Allow Spectators</Checkbox>
             </div> 
        </div>
