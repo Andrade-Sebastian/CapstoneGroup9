42a7ff150a08b974ed1065e3a0e78188bda3724e
Eman <epelayo480@gmail.com>
2025-02-14 14:12:34 -0800

    Made functions for database session creation, joining sessions, adding devices, configuring devices, getting session info, gettiing session users, checking if session is available

--- database/WB_Database_Setup.sql
+++ database/WB_Database_Setup.sql
@@ -1,83 +1,120 @@
+
+
+--Author: Emanuelle Pelayo
+--Purpose: Create the database
+
+
+--Done Refactoring
 CREATE TABLE IF NOT EXISTS Experiment
 (
     ExperimentID serial PRIMARY KEY,
     Name varchar(100) NOT NULL,
-    Description varchar(100) NOT NULL
+    Description varchar(255) --Nullable
 );
 
+--Done Refactoring
 CREATE TABLE IF NOT EXISTS PhotoLab
 (
     PhotoLabID serial PRIMARY KEY,
     ExperimentID int REFERENCES Experiment(ExperimentID),
     Path varchar(100) NOT NULL,
-    Captions varchar(100) NOT NULL
+    Captions varchar(100) --Nullable
 );
 
+--Done Refactoring
 CREATE TABLE IF NOT EXISTS Session
 (
     SessionID serial PRIMARY KEY,
     ExperimentID int REFERENCES Experiment(ExperimentID),
-    SessionCode varchar(100) NOT NULL,
     RoomCode varchar(100) NOT NULL,
     HostSocketID varchar(100) NOT NULL,
-    Users JSON ARRAY,
-    isInitialized BOOLEAN NOT NULL,
-    Configuration JSON NOT NULL,
-    Credentials JSON,
-    DiscoveredDevices JSON ARRAY,
-    SessionAvailable BOOLEAN NOT NULL
+    StartTimestamp TIMESTAMP, --Nullable
+    isPasswordProtected BOOLEAN,
+    Password VARCHAR(100), --Nullable
+    isSpectatorsAllowed BOOLEAN,
+    EndTimestamp TIMESTAMP --Nullable
 );
 
-CREATE TABLE IF NOT EXISTS AssociatedDevice
+--Done Refactoring
+CREATE TABLE IF NOT EXISTS Device
 (
-    AssociatedDevice serial PRIMARY KEY,
+    DeviceID serial PRIMARY KEY,
     IPAddress varchar(100) NOT NULL,
     SerialNumber varchar(100) NOT NULL,
-    SocketDeviceID varchar(100) NOT NULL,
-    SamplingFrequency int NOT NULL DEFAULT 50,
-    FrontEndSocketID varchar(100) NOT NULL,
-    IsAvailable BOOLEAN NOT NULL
+    DeviceSocketID varchar(100) NOT NULL,
+    SamplingFrequency int NOT NULL DEFAULT 50, --Change if needed
+    IsAvailable BOOLEAN NOT NULL,
+    IsConnected BOOLEAN --nullable
 );
+
 
 CREATE TABLE IF NOT EXISTS "User"
 (
     UserID serial PRIMARY KEY,
-    Name varchar(100) NOT NULL,
-    AssociatedDevice int REFERENCES AssociatedDevice(AssociatedDevice),
+    Nickname varchar(100) NOT NULL,
+    Device int REFERENCES Device(DeviceID), --nullable
     SessionID int REFERENCES Session(SessionID),
-    ExpirationData int NOT NULL DEFAULT 50
+    isMasked BOOLEAN,
+    FrontendSocketID VARCHAR(100),
+    LeftSession TIMESTAMP, --nullable
+    UserRole VARCHAR(100),
+    Secret VARCHAR(100)
 );
 
-
-CREATE TABLE IF NOT EXISTS AncilliaryDataFrame
+--Done Refactoring
+CREATE TABLE IF NOT EXISTS AncillaryDataFrame
 (
-    AncilliaryDataFrameID serial PRIMARY KEY,
+    AncillaryDataFrameID serial PRIMARY KEY,
     Package int,
     EDA numeric,
     Temperature numeric,
     Thermistor numeric
 );
 
-CREATE TABLE IF NOT EXISTS AuxilliaryDataFrame
+--Done Refactoring
+CREATE TABLE IF NOT EXISTS AuxiliaryDataFrame
 (
-    AuxilliaryDataFrameID serial PRIMARY KEY,
+    AuxDataFrameID serial PRIMARY KEY,
     Package int,
     PPG_Red numeric,
-    PPG_InfaRed numeric,
+    PPG_InfraRed numeric,
     PPG_Green numeric
 );
 
+--Done Refactoring
 CREATE TABLE IF NOT EXISTS Recording
 (
-    RecordingID serial PRIMARY KEY
+    RecordingID serial PRIMARY KEY,
+    ExpirationTimestamp TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS IntersectionTable
+--Done Refactoring
+CREATE TABLE IF NOT EXISTS RecordingDataPoint
 (
     DataFrameID serial PRIMARY KEY,
-    AncDataFrameID integer REFERENCES AncilliaryDataFrame(AncilliaryDataFrameID),
-    AuxDataFrameID integer REFERENCES AuxilliaryDataFrame(AuxilliaryDataFrameID),
+    UserID SERIAL REFERENCES "User" (UserID),
+    AncDataFrameID integer REFERENCES AncillaryDataFrame(AncillaryDataFrameID),
+    AuxDataFrameID integer REFERENCES AuxiliaryDataFrame(AuxDataFrameID),
     RecordingID int REFERENCES Recording(RecordingID),
-    Timestamp int NOT NULL
+    Timestamp TIMESTAMP
 );
 
+
+--Done Refactoring
+CREATE TABLE IF NOT EXISTS VideoLab
+(
+    VideoLabID SERIAL PRIMARY KEY,
+    ExperimentID SERIAL REFERENCES Experiment(ExperimentID),
+    Path VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS GalleryLab
+(
+    GalleryLabID INTEGER PRIMARY KEY,
+    ExperimentID INTEGER REFERENCES Experiment(ExperimentID),
+    Filename VARCHAR(256),
+    Path VARCHAR(256),
+    Caption VARCHAR(512),
+    TimeShown INTEGER,
+    ImageOrder INTEGER
+);
