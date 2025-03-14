1b6f0c9c55d5768172fca983d30a158a7a621308
Andrade-Sebastian <andradesebastian97@gmail.com>
2025-03-14 12:14:29 -0700

    uncommented import for article lab

--- host-electron/src/renderer/src/main.tsx
+++ host-electron/src/renderer/src/main.tsx
@@ -15,7 +15,7 @@ import PreviouslyMadeExperiments from './Views/PreviouslyMadeExperiments';
 import PhotoLab from './Views/PhotoLab';
 import VideoLab from './Views/VideoLab';
 import GalleryLab from './Views/GalleryLab';
-// import ArticleLab from './Views/ArticleLab';
+import ArticleLab from './Views/ArticleLab';
 import WaitingRoom from './Views/WaitingRoom';
 import ActivityRoom from './Views/ActivityHost';
 import Summary from './Views/Summary';
