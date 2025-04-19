db057c0ead7c7e0f05db295c62464ceadbffc64a
Eman Pelayo <98251644+emanp@users.noreply.github.com>
2025-04-18 17:10:53 -0700

    Revert to ce1b994ccf767f0c21f486d06fc24e11577dcd1

--- host-electron/src/renderer/src/Views/WaitingRoom.tsx
+++ host-electron/src/renderer/src/Views/WaitingRoom.tsx
@@ -8,7 +8,7 @@ import { PiCrownSimpleThin } from 'react-icons/pi'
 import { HiOutlineSignal } from 'react-icons/hi2'
 import { CiUser } from 'react-icons/ci'
 import { FaUserXmark } from "react-icons/fa6";
-import socket from './socket.tsx'
+import socket from './socket'
 import axios from 'axios'
 import { Divider } from '@heroui/divider'
 import WaitingRoomCardComponent from '../components/WaitingRoomCardComponent'
@@ -57,9 +57,6 @@ export default function WaitingRoom() {
   const [socketID, setSocketID] = useState('');
   const [theUserMap, setTheUserMap] = useState(new Map());
   const [focusedUser, setFocusedUser] = useState('')
-  const [focusedDeviceSerial, setFocusedDeviceSerial] = useState("")
-  const [focusedDeviceIP, setFocusedDeviceIP] = useState("")
-  const [focusedAssociatedUser, setFocusedAssociatedUser] = useState("");
   const [experimentIcon, setExperimentIcon] = useState<JSX.Element>(
     <CiPlay1 style={{ fontSize: '20px' }} />
   )
@@ -274,38 +271,25 @@ export default function WaitingRoom() {
 
   // Joined EmotiBit Settings Modal
   const handleRemoveEmoti = async() => {
-    console.log(selectedEmotiBitId + "selectedEmotiBitId")
-    // if(!selectedEmotiBitId) return;
+    if(!selectedEmotiBitId) return;
 
-    console.log("(handleRemoveEmoti): emotibit serial: " + focusedDeviceSerial + " IP: " + focusedDeviceIP)
     await axios.post(`http://localhost:3000/host/remove-device`, {
-      serialNumber: focusedDeviceSerial,
-      ipAddress: focusedDeviceIP
+      serialNumber: serialNumber,
+      ipAddress: IPAddress
     })
-    console.log("Removed emotibit with serial " + focusedDeviceSerial + " and deviceIP" + focusedDeviceIP )
     
     console.log("after axios")
     console.log("removing emotibit")
 
-    const serialNumberToRemove = focusedDeviceSerial;
+    const serialNumberToRemove = serialNumber;
 
     setCurrentDevices(currentDevices => 
       currentDevices.filter(device => device.associatedDevice.serialNumber !== serialNumberToRemove)
     );
     setIsModalOpenSettings(false);
   } 
-  const handleOpenModalSettings = (userId: string, associatedDevice: JSON, wholeObj: JSON) => {
-    console.log("nruh" + JSON.stringify(associatedDevice));
-    setFocusedDeviceSerial(associatedDevice.serialNumber)
-    setFocusedDeviceIP(associatedDevice.ipAddress)
-    setFocusedAssociatedUser(wholeObj.nickname)
-    
-    if (!userId){
-      console.log("Selected emotibit is not associated to a user, userid is "+ userId)
-    }
-
+  const handleOpenModalSettings = (userId: string) => {
     setSelectedEmotiBitId(userId);
-    console.log("selected emotibit" + selectedEmotiBitId)
     setIsModalOpenSettings(true);
   };
 
@@ -535,8 +519,6 @@ export default function WaitingRoom() {
     setSessionID(useSessionStore.getState().sessionId)
     const fetchUsers = async () => {
       try {
-        console.log("devices: " + JSON.stringify(devices))
-        console.log(devices)
         // console.log('Trying to get users from session ' + sessionID);
         const response = await axios.get(`http://localhost:3000/joiner/room-users/${sessionID}`)
         const users = response.data.users //Array of IUser objects
@@ -637,7 +619,7 @@ export default function WaitingRoom() {
               currentDevices.map((device) => {
                 // if (device.nickname !== null && device.associatedDevice.isConnected === true){
                   return(
-                    <EmotiBitList key={device.userId} user={device} onAction={() =>{ handleOpenModalSettings(device.userid, device.associatedDevice, device); console.log("HERE" + JSON.stringify(device))}} />
+                    <EmotiBitList key={device.userId} user={device} onAction={() => handleOpenModalSettings(device.userid)} />
                   );
                 //}
               })
@@ -719,7 +701,7 @@ export default function WaitingRoom() {
         </div>
         <div className="mb-6">
           <label htmlFor="ipAddress" className="block text-sm font-medium text-gray-700 mb-2">
-            IP Address 
+            IP Address
           </label>
           <input
             type="text"
@@ -740,34 +722,28 @@ export default function WaitingRoom() {
         // button3="Update"
       >
         <div className="mb-6">
-          {/* //Put this styling back: className="block text-sm font-medium text-gray-700 mb-2" */}
-          <label htmlFor="associated-user" className="block text-sm font-medium text-gray-700 mb-2">
-            Associated To: {focusedAssociatedUser}
-          </label>
           <label htmlFor="serialNumber" className="block text-sm font-medium text-gray-700 mb-2">
-            Serial Number: {focusedDeviceSerial}
+            Serial Number
           </label>
-          <label htmlFor="ipAddress" className="block text-sm font-medium text-gray-700 mb-2">
-            IP Address: {focusedDeviceIP}
-          </label>
-          {/* <input
+          <input
             type="text"
             id="serialNumber"
             className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             value={serialNumber}
             onChange={(e) => setSerialNumber(e.target.value)}
-          /> */}
+          />
         </div>
         <div className="mb-6">
-          {/* //Put this styling back: className="block text-sm font-medium text-gray-700 mb-2" */}
-
-          {/* <input
+          <label htmlFor="ipAddress" className="block text-sm font-medium text-gray-700 mb-2">
+            IP Address
+          </label>
+          <input
             type="text"
             id="ipAddress"
             className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             value={IPAddress}
             onChange={(e) => setIPAddress(e.target.value)}
-          /> */}
+          />
         </div>
       </ModalComponent>
       <ModalComponent
--- host-electron/src/renderer/src/Views/socket.tsx
+++ host-electron/src/renderer/src/Views/socket.tsx
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client';
-const socket = io("http://localhost:3000", {
-    ackTimeout: 10000
-});
+const socket = io("http://localhost:3000");
 export default socket;
